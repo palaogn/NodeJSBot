dc33204f247bc04865814d63ddf1cebbc5aedd1b
kristina.stojakovic@gmail.com <kristina.stojakovic@gmail.com>
2017-06-26 19:16:47 +0200

    Movie API added

--- app.js
+++ app.js
@@ -117,7 +117,7 @@ function processMessage(event) {
 }
 
 function findMovie(userId, movieTitle) {
-  request("http://www.omdbapi.com/?type=movie&amp;t=" + movieTitle, function (error, response, body) {
+  request("http://www.omdbapi.com/?type=movie&t=" + movieTitle, function (error, response, body) {
     if (!error && response.statusCode === 200) {
       var movieObj = JSON.parse(body);
       if (movieObj.Response === "True") {
